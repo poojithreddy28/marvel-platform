87d3f08584f1e362818cfbbcfdd2ecdf04d58b13
Richie Singh <richie.amornchotsingh@gmail.com>
2025-02-22 00:46:58 -0600

    fix:New Response Functionality

--- templates/Chat/TextMessage/TextMessage.jsx
+++ templates/Chat/TextMessage/TextMessage.jsx
@@ -1,62 +1,28 @@
-import { Box, Grid, Typography } from '@mui/material';
+import { useEffect } from 'react';
+import { Box, Grid, Typography, Fade } from '@mui/material';
 import Image from 'next/image';
 import Prism from 'prismjs';
-import 'prismjs/components/prism-python'; // Add the required language
+import 'prismjs/components/prism-python';
 import 'prismjs/components/prism-javascript';
 import 'prismjs/components/prism-java';
-import 'prismjs/themes/prism-tomorrow.css'; // Choose your theme
-import { useEffect } from 'react';
+import 'prismjs/themes/prism-tomorrow.css'; // Syntax Highlighting Theme
+import { motion } from 'framer-motion';
 import emoji from 'remark-emoji';
 import remarkGfm from 'remark-gfm';
-import { motion } from 'framer-motion';
-
 import MemoizedReactMarkdown from '@/components/MemoizedMarkdown';
 import ImageURLs from '@/assets/urls';
-
 import CodeComponent from '../CodeComponent';
-
-
 import markdownStyles from './TextMessage.module.css';
-
 import styles from './styles';
-
-const TextMessage = (props) => {
-  const { isMyMessage, message } = props;
-
+const TextMessage = ({ isMyMessage, message }) => {
   useEffect(() => {
     Prism.highlightAll(); // Apply syntax highlighting
   }, [message]);
-
   return (
-    <>
-      {isMyMessage ? (
-        // User message with fade-in and fade-out effect
-        <Grid id="message" {...styles.mainGridProps(isMyMessage)}>
-          <Grid {...styles.messageWrapperProps(isMyMessage)}>
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              transition={{
-                duration: 0.5,
-                ease: [0.4, 0, 0.2, 1], // Material Design easing
-              }}
-            >
-              <Typography {...styles.messageProps()}>
-                <MemoizedReactMarkdown
-                  remarkPlugins={[remarkGfm, emoji]}
-                  components={{ code: CodeComponent }}
-                >
-                  {message}
-                </MemoizedReactMarkdown>
-              </Typography>
-            </motion.div>
-          </Grid>
-        </Grid>
-      ) : (
-        // AI message with fade-in and fade-out effect, avatar remains static
-        <Grid id="message" {...styles.mainGridProps(isMyMessage)}>
-          <Grid {...styles.messageWrapperProps(isMyMessage)}>
+    <Fade in>
+      <Grid id="message" {...styles.mainGridProps(isMyMessage)}>
+        <Grid {...styles.messageWrapperProps(isMyMessage)}>
+          {!isMyMessage && (
             <Box>
               <Image
                 width="38.74px"
@@ -66,35 +32,41 @@ const TextMessage = (props) => {
               />
             </Box>
           )}
-          <Typography {...styles.messageProps()}>
-            <MemoizedReactMarkdown
-              remarkPlugins={[remarkGfm, emoji]}
-              className={`${markdownStyles['markdown-body']} `}
-              components={{
-                code({ node, inline, className, children, ...props }) {
-                  const match = /language-(\w+)/.exec(className || '');
-                  return !inline && match ? (
-                    <pre className={`language-${match[1]}`}>
-                      <code className={`language-${match[1]}`} {...props}>
+          <motion.div
+            initial={{ opacity: 0, y: isMyMessage ? 10 : -10 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: isMyMessage ? -10 : 10 }}
+            transition={{ duration: 0.9, ease: 'easeInOut' }}
+          >
+            <Typography {...styles.messageProps()}>
+              <MemoizedReactMarkdown
+                remarkPlugins={[remarkGfm, emoji]}
+                className={`${markdownStyles['markdown-body']} `}
+                components={{
+                  // eslint-disable-next-line react/no-unstable-nested-components
+                  code({ node, inline, className, children, ...props }) {
+                    const match = /language-(\w+)/.exec(className || '');
+                    return !inline && match ? (
+                      <pre className={`language-${match[1]}`}>
+                        <code className={`language-${match[1]}`} {...props}>
+                          {children}
+                        </code>
+                      </pre>
+                    ) : (
+                      <code className={className} {...props}>
                         {children}
                       </code>
-                    </pre>
-                  ) : (
-                    <code className={className} {...props}>
-                      {children}
-                    </code>
-                  );
-                },
-              }}
-            >
-              {message}
-            </MemoizedReactMarkdown>
-          </Typography>
-
+                    );
+                  },
+                }}
+              >
+                {message}
+              </MemoizedReactMarkdown>
+            </Typography>
+          </motion.div>
         </Grid>
-      )}
-    </>
+      </Grid>
+    </Fade>
   );
 };
-
 export default TextMessage;
